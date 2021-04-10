ddb605e079c9eec40bb5be429a5ac5c510346c72
sinhvien <sinhvien>
2021-04-10 09:40:13 +0700

    Test

---
+++ Web MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_MVC.Startup))]
+namespace Web_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
